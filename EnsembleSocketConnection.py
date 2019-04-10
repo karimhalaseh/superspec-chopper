bcd4519a6807a4eef8bcc50b5a55065b12323775
karimhalaseh <karimhalaseh241198@gmail.com>
2019-04-10 16:47:03 -0700

    Bug fixes, most todo's completed, style improved

--- EnsembleSocketConnection.py
+++ EnsembleSocketConnection.py
@@ -1,128 +1,112 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-EnsembleSocketConnection.py
-
-Program creates class to interact with Ensemble controller (and thus AGR-75 
+Program creates class to interact with Ensemble controller (and thus AGR-75
 rotary stage) with an ASCII interface through a socket-based connection. 
-This enables cross-platform use and no reliance on Aerotech 
-dependencies/libraries.
+Functionality specific to SuperSpec.
 
-Controller is assigned as server, PC is assigned as client. Wired connection
-must be via Ethernet. For the program to work:
+Controller is assigned as server, PC is assigned as client.
+For the program to work:
     
     -- Controller --
     (These tasks must be done in the Ensemble Configuration Manager, and so 
     must be done in Windows before moving to a different platform)
-    1) In Parameters->System->ASCII change the following parameters:
-	CommandSetup, enable Ethernet Socket 2
-    2) In Parameters->System->Ethernet Sockets change the following:
-	Socket2Port to 8000
-	Socket2Setup to TCP server
+    1) In Parameters->System->Communication->ASCII change the following
+    parameters:
+    CommandSetup, enable Ethernet Socket 2
+    2) In Parameters->System->Communication->Ethernet Sockets change
+    the following:
+    Socket2Port to 8000
+    Socket2Setup to TCP server
     
     -- This file --
     1) Change the controller_ip to be the IP of the controller
     2) Change the controller_port to match Socket2Port specified above (8000)
-    
-Class has functionality specific to the SuperSpec project.
 
 See Ensemble Help files for further documentation on the ASCII command 
 interface and on AeroBasic commands.
-
-@author: karimhalaseh
 """
 
 import socket
 import time
 import sys
 
-class Chopper:
-    # the IP of the controller
-    __controller_ip = ""
-    # the port of the controller
-    __controller_port = 8000
-    # the socket that will be used to communicate
-    __client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Parameters for motion
-    ON_AXIS = "1"
-    OFF_AXIS = "0"
-    SPEED = "80"
-    DWELL_TIME = 0.2
-    # TODO: input these into string commands - test if there is a time penalty 
-    # for doing so first though
-    
-    # ASCII controller response characters
-    SUCCESS = "%"
-    INVALID = "!"
-    FAULT = "#"
-    TIMEOUT = "$"
 
+class Chopper:
+    __controller_ip = "192.168.1.16"
+    __controller_port = 8000
+    __client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    DATA_BUFFER = 4096  # ASCII interface acknowledges command with a char
+    RESPONSE_DICT = {'%': 'Success', '!': 'Invalid command',
+                     '#': 'Axis at fault', '$': 'Command timed out'}
 
     # constructor opens socket to controller and enables motion 
     def __init__(self):
         
         try:
-            self.__client_socket.connect(self.__controller_ip, 
-                                         self.__controller_port)
-            self.__client_socket.sendall("ENABLE A\n")
-            response = self.__client_socket.recv(1)
-            if (response == self.SUCCESS):
-                print ("Connection successful\nMotion Enabled")
+            self.__client_socket.connect((self.__controller_ip,
+                                         self.__controller_port))
+            # message encoding required for Python 3, not Python 2
+            self.__client_socket.sendall("ENABLE A\n".encode())
+            response = self.RESPONSE_DICT.get(self.__client_socket.recv(
+                self.DATA_BUFFER).decode()[0])
+            if response == 'Success':
+                print("Connection successful\nMotion Enabled")
             else:
-                print ("Error: code - ", response)
-                # TODO: dictionary of response characters
-            
-            # SCurve controls curvature of velocity profile on % scale
-            self.__client_socket.sendall("SCURVE 100\n")
-            self.__client_socket.recv(1)
+                print(response)
             
         except:
-            print ("Error occurred: ", sys.exc_info()[0])
+            print("Error occurred: ", sys.exc_info()[0])
             print("Ensure controller is mapped and configured")
-            
-            
-    # chops between on and off axis positions        
-    def chop(self):
-        while True:
-            # chop to on-axis position of 1 deg
-            self.__client_socket.sendall("MOVEABS A 1 F 80\n")
-            self.__client_socket.recv(1)
-            time.sleep(self.DWELL_TIME)
-            # TODO: multithreading to not kill process during sleep
-            
-            # chop to off-axis position of 0 deg
-            self.__client_socket.sendall("MOVEABS A 0 F 80\n")
-            self.__client_socket.recv(1)
-            time.sleep(self.DWELL_TIME)
-    
-    
+
+    # chops between on and off axis positions
+    def chop(self, on_axis='1', off_axis='0', speed='80', dwell_time=0.2):
+        # WARNING - no command checking done to minimize latency
+        # Use on-axis() and off-axis() to check if errors occur
+        # TODO: implement loop breaking if required?
+        # while True:
+        self.__client_socket.sendall(("MOVEABS A " + on_axis + " F "
+                                      + speed + "\n").encode())
+        self.__client_socket.recv(self.DATA_BUFFER)
+        time.sleep(dwell_time)
+
+        self.__client_socket.sendall(("MOVEABS A " + off_axis + " F "
+                                      + speed + "\n").encode())
+        self.__client_socket.recv(self.DATA_BUFFER)
+        time.sleep(dwell_time)
+
     # moves to on axis position
-    def on_axis(self):
-        self.__client_socket.sendall("MOVEABS A 1 F 80\n")
-        if (self.__client_socket.recv(1) == self.SUCCESS):
-            print ("Chopper on-axis")
-        
-    
+    def on_axis(self, on_axis='1', speed='80'):
+        self.__client_socket.sendall(("MOVEABS A " + on_axis + " F "
+                                      + speed + "\n").encode())
+        response = self.RESPONSE_DICT.get(self.__client_socket.recv(
+            self.DATA_BUFFER).decode()[0])
+        if response != 'Success':
+            print(response)
+
     # moves to off axis position
-    def off_axis(self):
-        self.__client_socket.sendall("MOVEABS A 0 F 80\n")
-        if (self.__client_socket.recv(1) == self.SUCCESS):
-            print ("Chopper off-axis")
-    
-    
+    def off_axis(self, off_axis='1', speed='80'):
+        self.__client_socket.sendall(("MOVEABS A " + off_axis + " F "
+                                      + speed + "\n").encode())
+        response = self.RESPONSE_DICT.get(self.__client_socket.recv(
+            self.DATA_BUFFER).decode()[0])
+        if response != 'Success':
+            print(response)
+
+    # aborts motion
+    def stop(self):
+        self.__client_socket.sendall("ABORT A\n".encode())
+        response = self.RESPONSE_DICT.get(self.__client_socket.recv(
+            self.DATA_BUFFER).decode()[0])
+        if response != 'Success':
+            print(response)
+
     # disable motion and close socket
     def terminate(self):
-        self.__client_socket.sendall("DISABLE A\n")
-        if (self.__client_socket.recv(1) == self.SUCCESS):
-            print ("Motion disabled")
+        self.__client_socket.sendall("DISABLE A\n".encode())
+        response = self.RESPONSE_DICT.get(self.__client_socket.recv(
+            self.DATA_BUFFER).decode()[0])
+        if response == 'Success':
+            print("Motion disabled")
+        else:
+            print(response)
         self.__client_socket.close()
-    
-    
-    #TODO: increase overall error handling
-    
-        
-        
-    
-    
-    
